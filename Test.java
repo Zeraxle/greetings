94dfe7b4507afee1320879bef8f85e31ef2715a2
Zeraxle <code2gain@gmail.com>
2024-02-21 18:36:35 -0500

    initial Java assignment

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My name is Nathan Marrow");
+        System.out.println("I am 32 years old");
+        System.out.println("My hometown is High Point, NC");
+    }
+}
